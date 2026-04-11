b6a6e0dbea1ca7ab978e257a8edf20f71c31084a
Sia <siavash_p@hotmail.com>
2026-04-11 14:33:41 -0700

    Added two packages 'react-router-dom', and 'react-ga4' to setup google analytics as a utility

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
 import './App.css'
 import { ChatWindow } from './components/ChatWindow'
+import { initGA, logPageView } from './utils/google/analytics';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+
+  const location = useLocation();
+
+  useEffect(()=>{
+    initGA();
+  },[]);
+
+  useEffect(()=>{
+    logPageView(location.pathname + location.search )
+  },[location]);
 
   return (
     <>      
