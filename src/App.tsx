2cd3b1d7a315fef490458e89b6a23d6e6692d575
Sia <siavash_p@hotmail.com>
2026-04-11 14:39:27 -0700

    Remove 'react-router-dom', instead using window.location

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,17 @@ import { useEffect } from 'react';
 import './App.css'
 import { ChatWindow } from './components/ChatWindow'
 import { initGA, logPageView } from './utils/google/analytics';
-import { useLocation } from 'react-router-dom';
 
-function App() {
 
-  const location = useLocation();
+function App() { 
 
   useEffect(()=>{
     initGA();
   },[]);
 
   useEffect(()=>{
-    logPageView(location.pathname + location.search )
+    const path = window.location.pathname + window.location.search;
+    logPageView(path )
   },[location]);
 
   return (
