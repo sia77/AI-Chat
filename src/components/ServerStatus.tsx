2b01c0a6a18f5e9463a325fdb2ff7ae6484da9c1
Sia <siavash_p@hotmail.com>
2026-04-12 21:44:49 -0700

    switched to h-screen

--- src/components/ServerStatus.tsx
+++ src/components/ServerStatus.tsx
@@ -6,10 +6,10 @@ interface ServerStatusProps {
 
 export const ServerStatus = ({isServerLive}:ServerStatusProps) => {
 
-    if(isServerLive) return null;
+    if(!isServerLive) return null;
 
     return (
-        <div className="flex items-center justify-center min-h-dvh">
+        <div className="flex items-center justify-center h-screen">
             <div className="z-50 rounded bg-black/10 p-[30px] flex flex-col items-center gap-4">
                 <div className="flex gap-1">
                     {/* Three pulsing dots */}
