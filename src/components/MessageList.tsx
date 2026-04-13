a6837dbc6b4a8ffe63fb50231c8e5445f3d88aac
Sia <siavash_p@hotmail.com>
2026-04-12 21:40:40 -0700

    Added a auto scrolling

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import type { Message } from "../shared/types";
 import { MessageItem } from "./MessageItem";
 import { v4 as uuid } from 'uuid';
@@ -8,6 +9,17 @@ type MessageListProps = {
 
 export const MessageList = ({messageList}:MessageListProps) => {
 
+    const endOfMessage = useRef<HTMLDivElement>(null);
+
+    const scrollToBotton = () => {
+        endOfMessage.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
+    useEffect(()=>{
+        scrollToBotton();
+        console.log("it scrolling");
+    }, [messageList]);
+
     return (
         <div className="flex-1 overflow-y-auto p-4">
             <section className="max-w-[800px] mx-auto space-y-6">
@@ -15,7 +27,9 @@ export const MessageList = ({messageList}:MessageListProps) => {
                     messageList.map((msg)=> <MessageItem key = {uuid()} message = {msg} />)
                 }                
             </section>
+            <div ref={endOfMessage}></div>
         </div>
+        
     )
 }
 
